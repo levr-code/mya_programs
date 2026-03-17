53f53e804db4db0fac79e050fbb1262706bab814
levr-code <lev.reimarov@gmail.com>
2026-03-17 21:14:45 +0300

    Add mya_normal_input to easy_mya_funcs.c
    also fixed mya_normal_file_read

--- project_normal/easy_mya_funcs.c
+++ project_normal/easy_mya_funcs.c
@@ -1,7 +1,7 @@
 #include "../lib/myaos.h"
 
-char[] mya_normal_file_read(char[] name) {
-    static uint8_t buf[2048 + 1];
+const char* mya_normal_file_read(char[] name) {
+    uint8_t buf[2048 + 1];
     uint32_t size = 0;
 
     int c = mya_fs_read(name, buf, 2048, &size);
@@ -10,4 +10,14 @@ char[] mya_normal_file_read(char[] name) {
     };
     buf[size] = '\0';
     return (const char*)buf
+};
+
+const char* mya_normal_input(chat[] prompt){
+    mya_puts(prompt);
+    buf[100];
+    int n = mya_console_readline(buf, sizeof(buf), 1);
+    if (n != 0){
+        return NULL;
+    };
+    return (const char*)buf;
 };
