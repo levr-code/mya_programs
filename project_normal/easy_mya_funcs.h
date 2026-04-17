66496d48c46af6c8c742fdc76ef4f10d48274044
Lev Reimarov <lev.reimarov@gmail.com>
2026-04-17 17:15:19 +0300

    Made .h file consistent with .c file
    renamed mya_normal_input to mya_input_new, making it consistent with calling conventions

--- project_normal/easy_mya_funcs.h
+++ project_normal/easy_mya_funcs.h
@@ -2,5 +2,5 @@
 #define EASY_MYA_FUNCS_H
 
 const char* mya_file_read_new(char[] name, uint32_t size);
-const char* mya_normal_input(chat[] prompt)
+const char* mya_input_new(char[] prompt);
 #endif
